daed0a3d3d16a69b77ad1802d65d8888f5addfe8
zemikone <zemikone@gmail.com>
2019-04-28 20:01:50 +0530

    Create 5. Switch.java

---
+++ 5. Switch.java
@@ -0,0 +1,14 @@
+
+        switch (getFlg)
+        {
+            case "1":
+                shopType(frag_dialcom);
+                break;
+            default:
+                Toast.makeText(getApplicationContext(),"Select a Store!",Toast.LENGTH_SHORT).show();
+                break;
+
+
+        }
+        
+        
