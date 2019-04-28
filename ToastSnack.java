5c4afad93bfd712d0bf237762f0ad9a4de41e86b
zemikone <zemikone@gmail.com>
2019-04-28 19:47:06 +0530

    Create ToastSnack.java

---
+++ ToastSnack.java
@@ -0,0 +1,9 @@
+//Snack Bar
+
+ Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+//Toast
+
+Toast.makeText(getApplicationContext(), "Your toast message.", Toast.LENGTH_SHORT).show();
