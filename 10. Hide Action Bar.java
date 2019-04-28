41772b34a1b0b09d17ffcbb78542a5c098ce9b93
zemikone <zemikone@gmail.com>
2019-04-28 20:04:50 +0530

    Create 10. Hide Action Bar.java

---
+++ 10. Hide Action Bar.java
@@ -0,0 +1,9 @@
+
+//In the manifest under the relevent activity
+
+           <activity
+            android:name=".MainActivity"
+            android:theme="@style/Theme.AppCompat.NoActionBar"
+            > 
+            
+            
